2854bc9b9e23ce98ac036ae8542be040f9f9e3a9
haider2136 <haider2136@hotmail.com>
2017-07-11 01:11:00 +0100

    Add files via upload

--- Spot The difference/scripts.js
+++ Spot The difference/scripts.js
@@ -1,5 +1,9 @@
 
 
 
-var getElementById("Grapevine").onclick= function(
-innerHTML.getElementById("found") = "u clicked grapevine";) 
+/*var getElementById("Grapevine").onclick= function(
+innerHTML.getElementById("found") = "u clicked grapevine";) */
+
+/*$('#Differencemap').click(function() { 
+	innerHTML.getElementById("found") = "u clicked grapevine"; }*/
+document.getElementById(Grapevine).onclick=document.write("Found 1/3");
