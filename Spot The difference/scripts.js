0f72b6ecb00c20a819c34570ea5844985867df2f
haider2136 <haider2136@hotmail.com>
2017-07-11 12:52:59 +0100

    Add files via upload

--- Spot The difference/scripts.js
+++ Spot The difference/scripts.js
@@ -1,9 +1,18 @@
 
-
+/*alert("Hello");*/
 
 /*var getElementById("Grapevine").onclick= function(
 innerHTML.getElementById("found") = "u clicked grapevine";) */
 
 /*$('#Differencemap').click(function() { 
 	innerHTML.getElementById("found") = "u clicked grapevine"; }*/
-document.getElementById(Grapevine).onclick=document.write("Found 1/3");
+/*document.getElementById("Grapevine").onclick=document.write("Found 1/3");
+
+
+document.getElementById(EasyBut).onclick.display(EasyD);*/
+
+document.getElementById("Grapevine").onclick = function() {
+  
+    document.getElementById("found").innerHTML = "u found the difference!";
+  
+}
